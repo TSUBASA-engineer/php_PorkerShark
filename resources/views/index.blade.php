954eb5d42a28d4b657908e3a27280d5833470751
TSUBASA <onozawa.tsubasa@gmail.com>
2021-02-18 17:17:20 +0900

    header完成(ロゴ画像はいれてない)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/index_style.css') }}">
+
     <title>トップページ</title>
 </head>
 <body>
+    <nav class="site-header sticky-top py-1">
+        <div class="container d-frex-colm flex-md-row text-center">
+            <a href="#">ロゴ</a>
+        </div>
+    </nav>
     <h1>トップページ</h1>
 </body>
 </html>
